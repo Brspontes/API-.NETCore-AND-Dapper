f62d0fe8ea65292b1648ff2f4c6d97ffc0e14431
Brian Pontes <brian.robert16@hotmail.com>
2019-06-04 22:48:04 -0300

    Create a new project and setting up structure

---
+++ BrsPontes.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BrsPontes.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ BrsPontes.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BrsPontes.Shared
+{
+    public class Class1
+    {
+    }
+}
