80342057700de8337d5b1d538397a324cb69e290
Brian Pontes <brian.robert16@hotmail.com>
2019-06-08 19:32:08 -0300

    Adding tests

---
+++ BrsPontes.Tests/DocumentTests.cs
@@ -0,0 +1,32 @@
+﻿using BrsPontes.Domain.StoreContext.ValuesObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrsPontes.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document validDocument;
+        private Document invalidDocument;
+
+        public DocumentTests()
+        {
+            validDocument = new Document("51693572060");
+            invalidDocument = new Document("123456789");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.AreEqual(false, invalidDocument.Valid);
+        }
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {
+            Assert.AreEqual(true, validDocument.Valid);
+        }
+    }
+}
