5e7bae93d9984ffd68767f6310c128162144f739
Brian Pontes <brian.robert16@hotmail.com>
2019-06-08 17:10:21 -0300

    Adding Validations

--- BrsPontes.Domain/StoreContext/ValuesObjects/Email.cs
+++ BrsPontes.Domain/StoreContext/ValuesObjects/Email.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using FluentValidator;
+using FluentValidator.Validation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BrsPontes.Domain.StoreContext.ValuesObjects
 {
-    public class Email
+    public class Email : Notifiable
     {
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new ValidationContract().Requires()
+                .IsEmail(Address, "Email", "O e-mail é inválido"));
         }
 
         public string Address { get; private set; }
--- BrsPontes.Domain/StoreContext/ValuesObjects/Name.cs
+++ BrsPontes.Domain/StoreContext/ValuesObjects/Name.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using FluentValidator;
+using FluentValidator.Validation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BrsPontes.Domain.StoreContext.ValuesObjects
 {
-    public class Name
+    public class Name : Notifiable
     {
         public Name(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
+
+            AddNotifications(new ValidationContract().Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos três caracteres")
+                .HasMaxLen(firstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres")
+                .HasMinLen(lastName, 3, "LastName", "O sobrenome deve conter no minimo 3 caracteres")
+                .HasMaxLen(firstName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres"));
         }
 
         public string FirstName { get; private set; }
